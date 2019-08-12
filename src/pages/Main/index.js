0b647a7817e1be6c275296fd97a20607d109fa23
phabiulla <phabiulla@gmail.com>
2019-08-12 20:11:51 +0100

    12.08: Importação inicial do front-end web da aplicação git-repositorys local para repositório git.

---
+++ src/pages/Main/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositório" />
+        <SubmitButton disabled>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Main}></Route>
+        <Route path="/repository" component={Repository}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
